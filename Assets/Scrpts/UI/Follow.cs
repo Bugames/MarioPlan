662c1d38bbf7ceca60660a2b19eb830f34c61fb1
Ligo <1569731402@qq.com>
2020-11-11 20:57:34 +0800

    feat: 完成游戏整合
    完善动画，流程
    完成游戏整合

---
+++ Assets/Scrpts/UI/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+	public Transform target;
+
+    private void Update()
+    {
+        float sameX = target.position.x;
+        Vector3 position = transform.position;
+        position.x = sameX;
+        transform.position = position;
+    }
+
+}
